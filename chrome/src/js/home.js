75c88ea0b7b5b6f76c99caa4e7bb1c054d738b9c
Zhang Tai <q6112345@gmail.com>
2017-12-28 14:32:34 +0800

    Replace server domain to boost download speed

--- chrome/src/js/home.js
+++ chrome/src/js/home.js
@@ -96,7 +96,7 @@ class Home extends Downloader {
     for (let key in files) {
       this.fileDownloadInfo.push({
         name: files[key].path.substr(prefix),
-        link: `${location.protocol}//pcs.baidu.com/rest/2.0/pcs/file?method=download&app_id=250528&path=${encodeURIComponent(files[key].path)}`,
+        link: `${location.protocol}//www.baidupcs.com/rest/2.0/pcs/file?method=download&app_id=250528&path=${encodeURIComponent(files[key].path)}`,
         md5: files[key].md5
       })
     }
